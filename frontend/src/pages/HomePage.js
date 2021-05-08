bbf6a90b2d6cbdb49a15e7027f2b4cbb0f9cac62
deundrewilliams <dnamedno17@gmail.com>
2021-05-07 21:19:22 -0400

    frontend changes

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import axios from 'axios';
 
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+       var c = ca[i];
+       while (c.charAt(0) ===' ') c = c.substring(1);
+       if(c.indexOf(name) === 0)
+          return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+const options = {
+    headers: {"X-CSRFToken": getCookie('csrftoken')}
+}
+
 
 class HomePage extends React.Component {
 
     constructor() {
         super();
+
+
     }
 
 
+
+
 }
+
+export default HomePage;
