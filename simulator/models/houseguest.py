4e5841b5bf7775e37bca5bea771a7ed1abf04570
deundrewilliams <dnamedno17@gmail.com>
2021-05-16 21:32:10 -0400

    add relationship initialization and impact

--- simulator/models/houseguest.py
+++ simulator/models/houseguest.py
@@ -1,8 +1,15 @@
+import random
 from django.db import models
 
-# from .game import Game
 
 class Houseguest(models.Model):
+
+    NEUTRAL_RELATIONSHIP = 50
+
+    POSITIVE = 1
+    NEUTRAL = 2
+    NEGATIVE = 3
+
     name = models.TextField(blank=False, null=False)
     game = models.ForeignKey(to="Game", on_delete=models.CASCADE, related_name="players")
     immune = models.BooleanField(blank=False, null=False, default=False)
@@ -36,9 +43,33 @@ class Houseguest(models.Model):
         self.competition_count += 1
         self.save()
 
+    def initialize_relationships(self, houseguests):
 
+        self.relationships = {k:self.NEUTRAL_RELATIONSHIP for k in houseguests if k != self}
 
+    def impact_relationship(self, affected_houseguest, impact_level):
 
+        # POS: 0 to 4
+        # NEU: -2 to 2
+        # NEG: -4 to 0
 
+        # Set upper bound and lower bound
+        if impact_level == self.POSITIVE:
+            upper = 5
+            lower = 0
+        elif impact_level == self.NEUTRAL:
+            upper = 2
+            lower = -2
+        else:
+            upper = 0
+            lower = -5
 
+        # Generate a random number between upper and lower bound
+        impact_amount = random.randint(lower, upper)
+
+        # Edit self's relationship with affected houseguest
+        self.relationships[affected_houseguest] += impact_amount
+
+        # Edit affected houseguest's relationship with self
+        affected_houseguest.relationships[self] += impact_amount
 
--- simulator/unit_tests/test_houseguest.py
+++ simulator/unit_tests/test_houseguest.py
@@ -1,4 +1,5 @@
 import pytest
+import random
 from ..models import Houseguest
 from ..factories import HouseguestFactory, ContestantFactory, GameFactory
 
@@ -53,4 +54,83 @@ class TestHouseguest():
         hg.win_competition()
 
         assert hg.competition_count == 1
+
+    @pytest.mark.django_db
+    def test_initialize_relationships(self):
+
+        g = GameFactory()
+        hgs = HouseguestFactory.create_batch(6, game=g)
+
+        for hg in hgs:
+            hg.initialize_relationships(hgs)
+
+        expected_rels = {k:Houseguest.NEUTRAL_RELATIONSHIP for k in hgs if k != hgs[0]}
+
+        assert expected_rels == hgs[0].relationships
+
+    @pytest.mark.django_db
+    def test_impact_relationship_negative(self, monkeypatch):
+
+        hgs = HouseguestFactory.create_batch(2)
+
+        for hg in hgs:
+            hg.initialize_relationships(hgs)
+
+        def mock_randint(lower, upper):
+
+            assert lower == -5
+            assert upper == 0
+
+            return -2
+
+        monkeypatch.setattr(random, "randint", mock_randint)
+
+        hgs[0].impact_relationship(hgs[1], Houseguest.NEGATIVE)
+
+        assert hgs[0].relationships[hgs[1]] == Houseguest.NEUTRAL_RELATIONSHIP - 2
+        assert hgs[1].relationships[hgs[0]] == Houseguest.NEUTRAL_RELATIONSHIP - 2
+
+    @pytest.mark.django_db
+    def test_impact_relationship_positive(self, monkeypatch):
+
+        hgs = HouseguestFactory.create_batch(2)
+
+        for hg in hgs:
+            hg.initialize_relationships(hgs)
+
+        def mock_randint(lower, upper):
+
+            assert lower == 0
+            assert upper == 5
+
+            return 3
+
+        monkeypatch.setattr(random, "randint", mock_randint)
+
+        hgs[0].impact_relationship(hgs[1], Houseguest.POSITIVE)
+
+        assert hgs[0].relationships[hgs[1]] == Houseguest.NEUTRAL_RELATIONSHIP + 3
+        assert hgs[1].relationships[hgs[0]] == Houseguest.NEUTRAL_RELATIONSHIP + 3
+
+    @pytest.mark.django_db
+    def test_impact_relationship_neutral(self, monkeypatch):
+
+        hgs = HouseguestFactory.create_batch(2)
+
+        for hg in hgs:
+            hg.initialize_relationships(hgs)
+
+        def mock_randint(lower, upper):
+
+            assert lower == -2
+            assert upper == 2
+
+            return 1
+
+        monkeypatch.setattr(random, "randint", mock_randint)
+
+        hgs[0].impact_relationship(hgs[1], Houseguest.NEUTRAL)
+
+        assert hgs[0].relationships[hgs[1]] == Houseguest.NEUTRAL_RELATIONSHIP + 1
+        assert hgs[1].relationships[hgs[0]] == Houseguest.NEUTRAL_RELATIONSHIP + 1
 
